ea508e8453d9394a2b82cdb881ef3faad41e8a67
Mateusz Bobinski <mat.bobinski@gmail.com>
2014-01-25 12:31:21 +0100

    End of project

--- main.cpp
+++ main.cpp
@@ -2,13 +2,16 @@
 #include "generator.h"
 #include "min.h"
 #include "max.h"
+#include "utils.h"
 
 int main()
 {
 	int* tab = new int[10];
 	generate(tab);
+	printab(tab, 10);
 	std::cout << "Minimalna wartosc: " << min(tab, 10) << std::endl;
 	std::cout << "Maksymalna wartosc: " << max(tab, 10) << std::endl;
+	std::cout << "Suma: " << suma(tab, 10);
 	
 	int a;
 	std::cin >> a;
--- utils.h
+++ utils.h
@@ -1,13 +1,13 @@
 #include <iostream>
 
-int printab(int &a,unsigned int n)
+int printab(int *a,unsigned int n)
 {
 for(unsigned int i=0;i<n;i++)
-   std::cout << a[i] << endl;
+   std::cout << a[i] << std::endl;
 return 0;
 }
 
-int suma(int &a, unsigned int n)
+int suma(int *a, unsigned int n)
 {
  int sum = 0;
 
