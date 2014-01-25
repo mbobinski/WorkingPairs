f538140773c63dda2cccbda9f8e3c1d221f125b6
slw131 <slw131@wp.pl>
2014-01-25 12:17:27 +0100

    zad 3

--- generator.h
+++ generator.h
@@ -5,4 +5,6 @@ int generate(int &a)
 {
    for(unsigned int i=0;i<10;i++)
        a[i] = rand()%101;
+
+   return 0;
 }
---
+++ utils.h
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int printab(int &a,unsigned int n)
+{
+for(unsigned int i=0;i<n;i++)
+   std::cout << a[i] << endl;
+return 0;
+}
+
+int suma(int &a, unsigned int n)
+{
+ int sum = 0;
+
+ for (unsigned int i=0;i<n;i++)
+     sum += a[i];
+
+ return sum;
+
+}
