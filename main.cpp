622da6da771847cb3ddbbc820169c970111dca16
Mateusz Bobinski <mat.bobinski@gmail.com>
2014-01-25 12:31:57 +0100

    End of project

--- main.cpp
+++ main.cpp
@@ -12,9 +12,7 @@ int main()
 	std::cout << "Minimalna wartosc: " << min(tab, 10) << std::endl;
 	std::cout << "Maksymalna wartosc: " << max(tab, 10) << std::endl;
 	std::cout << "Suma: " << suma(tab, 10);
-	
-	int a;
-	std::cin >> a;
+
 	delete[] tab;
 	return 0;
 }
